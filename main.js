abf0996d8fdf385a67a4ffc79d5ecb7495320802
PRISCILA RUOTOLO <126161177+PRISCILARUOTOLO@users.noreply.github.com>
2026-09-02 14:59:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,5 +15,13 @@ function iniciarDesafio() {
     if (nome === "") {
         alert("Digite seu nome para começar!");
         return;
+
+    const numeros = Math.floor(Math.randon() * desafios.length);
+    const desafio = desafios[numeros];
+
+    document.getElementById("resposta").innerHTML=`
+    <h2> Olá ${name}</h2>`
+    `<h2> Seu Desafio é: ${desafio}</h2>`
+       
     }
 
